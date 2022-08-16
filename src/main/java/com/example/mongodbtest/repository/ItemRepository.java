9f1d45339790d9d3a13e267fa15e96bbd21f63c3
Rinse Willet <rinsewillet@rinses-air.kpn>
2022-08-16 08:14:45 +0200

    second

---
+++ src/main/java/com/example/mongodbtest/repository/ItemRepository.java
@@ -0,0 +1,22 @@
+package com.example.mongodbtest.repository;
+
+//Springboot
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+//Java
+import java.util.List;
+
+//Package component
+import com.example.mongodbtest.model.GroceryItem;
+
+public interface ItemRepository extends MongoRepository<GroceryItem, String> {
+
+    @Query("{name:'?0'}")
+    GroceryItem findItemByName(String name);
+
+    @Query(value="{category:'?0'}", fields = "{'name' : 1, 'quantity' : 1}")
+    List<GroceryItem> findAll(String category);
+
+    public long count();
+}
